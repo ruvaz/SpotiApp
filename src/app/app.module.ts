4262726cc4baf60b1f343b318a2165e08ce3e468
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2020-04-17 14:10:27 -0500

    http module ejemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {SearchComponent} from './components/search/search.component';
+import {ArtistaComponent} from './components/artista/artista.component';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
+
+
+// agregar router al proyecto
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
+
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES, {useHash: true}) // agregar router al proyecto
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
